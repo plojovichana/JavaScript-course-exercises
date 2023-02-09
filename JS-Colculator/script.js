b8434e07ad2284560d2e86df7c13d68b2bc7c3dc
Hana Plojovic <hanaplojovic123@gmail.com>
2023-02-09 10:42:59 +0100

    JS-course

--- JS-Colculator/script.js
+++ JS-Colculator/script.js
@@ -22,6 +22,7 @@ buttons.forEach((item) => {
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
+
 let isDark = true;
 themeToggleBtn.onclick = () => {
   calculator.classList.toggle("dark");
